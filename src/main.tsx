625e0feadeb406c6a0d9e2c8c4f3fdb7893b9aa7
Hamdi Kamel <hamdi.pirillo4@gmail.com>
2023-03-20 06:49:32 -0700

    removed unnecessary file includes

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClientProvider, QueryClient } from 'react-query'
-import { Provider } from 'react-redux'
-import Store from './Store'
 import App from './App'
 import './sass/main.scss'
 
@@ -11,9 +9,7 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Provider store={Store}>
-        <App />
-      </Provider>
+      <App />
     </QueryClientProvider>
   </React.StrictMode>,
 )
