6aa4f715bd60fdc53dceb43139759c2577678a36
Hamdi Kamel <hamdi.pirillo4@gmail.com>
2023-03-22 06:36:33 -0700

    improved folder structure and modulerized sass files

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     const imgs = [
         'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/35271885-b64b-49da-b43d-e9aa8fa4d471/deib63r-0ab3a0f9-f527-45fc-aa28-0e2520df03fd.jpg/v1/fill/w_1920,h_1080,q_75,strp/persona_5_strikers___yellow_wallpaper_by_thetruemask_deib63r-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTA4MCIsInBhdGgiOiJcL2ZcLzM1MjcxODg1LWI2NGItNDlkYS1iNDNkLWU5YWE4ZmE0ZDQ3MVwvZGVpYjYzci0wYWIzYTBmOS1mNTI3LTQ1ZmMtYWEyOC0wZTI1MjBkZjAzZmQuanBnIiwid2lkdGgiOiI8PTE5MjAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.yy8iADJSfnWFmfvmqFcQFGbaDuQH0qLIVPljlsbC-FQ',
         'https://i.redd.it/vry3uc5ok9w81.png',
-        'https://c4.wallpaperflare.com/wallpaper/524/879/605/video-game-persona-4-golden-persona-4-yu-narukami-wallpaper-preview.jpg',
+        'https://uploads-ssl.webflow.com/6074f6263548834cc1229762/63040b070806156a1fa88877_07.jpg',
         'https://moewalls.com/wp-content/uploads/2022/11/joker-persona-5-royal-thumb.jpg'
     ]
 
